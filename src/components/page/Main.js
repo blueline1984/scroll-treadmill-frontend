726f057506469b3d2f77c77c77e639ac5b4614d7
blueline1318 <blueline1318@gmail.com>
2022-07-04 23:36:09 +0900

    [FEAT]: Add mainpage icons & styled components

--- src/components/page/Main.js
+++ src/components/page/Main.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiOutlineVolumeOff, HiOutlineVolumeUp } from "react-icons/hi";
+import { AiOutlineEye } from "react-icons/ai";
+import { GrCircleInformation } from "react-icons/gr";
+
+import styled from "styled-components";
 
 function Main() {
-  return <div>Main</div>;
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <>
+      {isPlaying ? (
+        <button>
+          <HiOutlineVolumeUp />
+        </button>
+      ) : (
+        <button>
+          <HiOutlineVolumeOff />
+        </button>
+      )}
+      <button>
+        <AiOutlineEye />
+      </button>
+      <button>
+        <GrCircleInformation />
+      </button>
+      <MainTitle>Scroll Treadmill</MainTitle>
+      <SubTitle>Running game with mouse scroll event</SubTitle>
+    </>
+  );
 }
+
+const MainTitle = styled.div`
+  color: white;
+  margin-top: 10%;
+  font-size: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SubTitle = styled.div`
+  color: white;
+  margin-top: 0;
+  font-size: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Main;
