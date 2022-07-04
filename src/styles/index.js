b3c9db00ba93a30a65329c48fd763e7cb4ddf06b
blueline1318 <blueline1318@gmail.com>
2022-07-04 23:38:47 +0900

    [STYLE]: Apply bacground color & font

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from "styled-components";
+import "@fontsource/amatic-sc";
 
 const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    background-color: #A0BCC2;
+    font-family: "Amatic SC";
   }
 
   button {
