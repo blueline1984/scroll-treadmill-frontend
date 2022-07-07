3eff7829c45ea8d4094c9fdc477f55850869652b
blueline1318 <blueline1318@gmail.com>
2022-07-07 20:26:52 +0900

    [FEAT]: Add route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import GamePage from "../pages/GamePage";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
