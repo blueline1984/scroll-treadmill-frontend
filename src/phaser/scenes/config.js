a4e57d5f4332740b78e232bb7fb2df0c104ffb20
blueline1318 <blueline1318@gmail.com>
2022-07-08 15:48:48 +0900

    [FEAT]: Make timer

--- src/phaser/scenes/config.js
+++ src/phaser/scenes/config.js
@@ -1,16 +1,16 @@
 import Phaser from "phaser";
-import Single from "./SinglePlay";
+import SinglePlay from "./SinglePlay";
 
 const config = {
+  title: "Scroll Treadmill",
   type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER,
-  },
   parent: "phaser-container",
   backgroundColor: "#354259",
+  scale: {
+    mode: Phaser.Scale.ScaleModes.NONE,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
   physics: {
     default: "arcade",
     arcade: {
@@ -18,7 +18,8 @@ const config = {
       debug: true,
     },
   },
-  scene: [Single],
+  autoFocus: true, //조사 필요
+  scene: [SinglePlay],
 };
 
 export default config;
