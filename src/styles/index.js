448dc4960be62c24a02cf001477d1e1791520e80
blueline1318 <blueline1318@gmail.com>
2022-07-11 20:56:10 +0900

    [FEAT]: Add font style

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,12 +1,20 @@
 import { createGlobalStyle } from "styled-components";
-import "@fontsource/amatic-sc";
+import ActionJ from "../assets/font/actionj.ttf";
 
 const GlobalStyle = createGlobalStyle`
   * {
+    background-color: #A0BCC2;
+    font-family: "Amatic SC" , cursive;
+
+    @font-face {
+      font-family: ActionJ;
+      src: url(${ActionJ})
+    }
+  }
+
+  body {
     margin: 0;
     padding: 0;
-    background-color: #A0BCC2;
-    font-family: "Amatic SC";
   }
 
   button {
