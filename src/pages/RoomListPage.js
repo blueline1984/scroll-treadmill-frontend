e822d0314d356a9b9d40886f473545d6180edd25
blueline1318 <blueline1318@gmail.com>
2022-07-13 14:05:41 +0900

    [FEAT]: Show room list on the page

--- src/pages/RoomListPage.js
+++ src/pages/RoomListPage.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { socket } from "../utils/socket";
 
 function RoomListPage() {
+  const [roomLists, setRoomLists] = useState({});
   const navigate = useNavigate();
 
-  console.log(socket.id);
-
-  console.log(
-    socket.on("currentPlayerss", (players) => {
-      console.log(players);
-    })
-  );
+  //만들어진 방 목록 받기
+  useEffect(() => {
+    socket.on("roomList", (list) => {
+      setRoomLists(list);
+    });
+  }, [roomLists]);
 
   return (
     <>
-      <div>RoomListPage</div>
+      <h1>RoomListPage</h1>
       <button
         onClick={() => {
           navigate("/");
@@ -24,11 +24,15 @@ function RoomListPage() {
       >
         Back To Main
       </button>
-      <div style={{ display: "flex", border: "1px solid white" }}>
-        <h1>Room title</h1>
-        <p>player's number</p>
-        <button>Join</button>
-      </div>
+      <p>
+        {Object.keys(roomLists).map((roomId) => (
+          <p key={roomId} style={{ border: "solid 1px black" }}>
+            <h2>{roomLists[roomId].roomTitle}</h2>
+            <h2>Participant Number</h2>
+            <button>Join Button</button>
+          </p>
+        ))}
+      </p>
     </>
   );
 }
