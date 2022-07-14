058a2df4c508c30b2be0ec195dadbe78a917f674
blueline1318 <blueline1318@gmail.com>
2022-07-14 15:42:35 +0900

    [FIX]: Fix show roomlists with recoil

--- src/pages/RoomListPage.js
+++ src/pages/RoomListPage.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useParams, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
 
 import { socket } from "../utils/socket";
 
+import { roomlistState } from "../states/roomlist";
+
 function RoomListPage() {
   const [joinRoom, setJoinRoom] = useState("");
-  const [roomLists, setRoomLists] = useState({});
   const [participantNum, setParticipantNum] = useState(0);
+  const [roomListRecoil, setRoomListRecoil] = useRecoilState(roomlistState);
 
   const navigate = useNavigate();
 
@@ -20,10 +23,11 @@ function RoomListPage() {
 
   //만들어진 방 목록 받기
   useEffect(() => {
-    socket.on("roomList", (list) => {
-      setRoomLists(list);
+    socket.on("roomList", (rooms) => {
+      console.log("방 목록받기");
+      setRoomListRecoil(rooms);
     });
-  }, [roomLists]);
+  }, []);
 
   return (
     <>
@@ -36,11 +40,11 @@ function RoomListPage() {
         Back To Main
       </button>
       <div>
-        {Object.keys(roomLists).map((roomId) => (
+        {Object.keys(roomListRecoil).map((roomId) => (
           <li key={roomId} style={{ border: "solid 1px black" }}>
-            <p>{roomLists[roomId].roomTitle}</p>
+            <p>{roomListRecoil[roomId].roomTitle}</p>
             <p>
-              {participantNum} / {roomLists[roomId].roomMaxNum}
+              {participantNum} / {roomListRecoil[roomId].roomMaxNum}
             </p>
             {/* <Link to={`/waitingroom/${roomId}`}>button</Link> */}
             <button onClick={() => handleJoinRoom(roomId)}>Join Button</button>
