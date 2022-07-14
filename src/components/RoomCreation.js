9f5600213b139f138315aeaf3740cf41b538822e
blueline1318 <blueline1318@gmail.com>
2022-07-14 16:02:42 +0900

    [FIX]: Fix route to mainpage after making room

--- src/components/RoomCreation.js
+++ src/components/RoomCreation.js
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import { socket } from "../utils/socket";
+
+import { roomlistState } from "../states/roomlist";
 
 function RoomCreation() {
   const [roomTitle, setRoomTitle] = useState("");
+  const roomListRecoil = useRecoilValue(roomlistState);
+  console.log("여기에요", roomListRecoil);
+
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     setRoomTitle(event.target.value);
   };
 
-  const handleRoomCreation = (event) => {
-    event.preventDefault();
-    socket.emit("makeRoom", roomTitle);
-    navigate("/roomlist");
+  const handleRoomCreation = () => {
+    if (roomTitle.length === 0) {
+      alert("Please,Insert Room Title");
+    } else {
+      socket.emit("makeRoom", roomTitle);
+      navigate("/main");
+    }
   };
 
   return (
     <>
       <h1>Create Room</h1>
-      <form id="room">
-        <label htmlFor="title">Room's Title :</label>
-        <input type="text" id="title" onChange={handleInputChange} />
-        <label htmlFor="player">Players :</label>
-        <select id="cars" name="player" form="room">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <button onClick={handleRoomCreation}>방만들기</button>
-      </form>
+      <label htmlFor="title"> Room's Title : </label>
+      <input id="title" type="text" onChange={handleInputChange} />
+      <button onClick={handleRoomCreation}>Create</button>
     </>
   );
 }
