1ee50c717e9343ba72106d8e4e734ddc8127eebe
blueline1318 <blueline1318@gmail.com>
2022-07-22 17:14:24 +0900

    [FEAT]: Add loading page in multi mode

--- src/phaser/scenes/MultiPlay/MultiPlayScene.js
+++ src/phaser/scenes/MultiPlay/MultiPlayScene.js
@@ -15,36 +15,6 @@ export default class Multi extends Phaser.Scene {
     this.height = this.scale.height;
   }
 
-  preload() {
-    //loading
-    // this.setLoading();
-
-    //audio
-    this.loadBackgroundMusic();
-
-    //Alien
-    this.load.spritesheet("alien", `textures/alien2_spritesheet.png`, {
-      frameWidth: 68,
-      frameHeight: 93,
-    });
-
-    //Treadmill
-    this.load.spritesheet("treadmill", "textures/treadmill_spritesheet.png", {
-      frameWidth: 1800,
-      frameHeight: 750,
-    });
-
-    //Chracter identifier
-    this.load.image("me", "textures/me.png");
-
-    //loading
-    // this.load.on("progress", this.updateLoading, {
-    //   loadingText: this.loadingText,
-    // });
-
-    // this.load.on("complete", this.completeLoading, { scene: this.scene });
-  }
-
   create() {
     //audio
     this.inGameMusic = this.sound.add("ingame", { loop: true });
@@ -377,27 +347,27 @@ export default class Multi extends Phaser.Scene {
     });
   }
 
-  setLoading() {
-    const { width, height } = this.scale;
+  // setLoading() {
+  //   const { width, height } = this.scale;
 
-    this.loadingText = this.add
-      .text(width * 0.5, height * 0.4, "Loading...", {
-        fontSize: "200px",
-        fill: "#FFFFFF",
-        fontFamily: "Amatic SC",
-      })
-      .setOrigin(0.5);
-  }
+  //   this.loadingText = this.add
+  //     .text(width * 0.5, height * 0.4, "Loading...", {
+  //       fontSize: "200px",
+  //       fill: "#FFFFFF",
+  //       fontFamily: "Amatic SC",
+  //     })
+  //     .setOrigin(0.5);
+  // }
 
-  updateLoading(percentage) {
-    this.loadingText.setText(`Loading... ${percentage.toFixed(0) * 100}%`);
-  }
+  // updateLoading(percentage) {
+  //   this.loadingText.setText(`Loading... ${percentage.toFixed(0) * 100}%`);
+  // }
 
-  completeLoading() {
-    this.scene.start("multi");
-  }
+  // completeLoading() {
+  //   this.scene.start("multi");
+  // }
 
-  loadBackgroundMusic() {
-    this.load.audio("ingame", "sound/racing mode_2.mp3");
-  }
+  // loadBackgroundMusic() {
+  //   this.load.audio("ingame", "sound/racing mode_2.mp3");
+  // }
 }
