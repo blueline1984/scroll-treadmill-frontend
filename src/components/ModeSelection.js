c83cbd60900a3605273fcec1a1718005ade972f9
blueline1318 <blueline1318@gmail.com>
2022-07-25 17:12:21 +0900

    [STYLE]: Add main page information modal style

--- src/components/ModeSelection.js
+++ src/components/ModeSelection.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Modal from "./Modal";
 import RoomCreation from "./RoomCreation";
-
 import styled from "styled-components";
 
 function ModeSelection({ onBlindMode }) {
